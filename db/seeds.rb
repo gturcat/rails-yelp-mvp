42161c31d4c0f1c1b2fb45b2d8ae9f108304bf80
Guillaume Turcat <gturcat@me.com>
2019-02-13 15:48:22 +0100

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+chez_yves = Restaurant.new(name: "Chez Yves", address: "Avenue Robert Schuman", category: "french")
+chez_yves.save
+
+petit_nice = Restaurant.new(name: "Petit Nice", address: "Prado", category: "french")
+petit_nice.save
+
+chez_bras = Restaurant.new(name: "Chez Bras", address: "Laguiole", category: "french")
+chez_bras.save
+
+pekin_express = Restaurant.new(name: "Pekin express", address: "Cours Mirabeau", category: "chinese")
+pekin_express.save
+
+trattoria = Restaurant.new(name: "Trattoria", address: "Rue du Paradis", category: "italian")
+trattoria.save
